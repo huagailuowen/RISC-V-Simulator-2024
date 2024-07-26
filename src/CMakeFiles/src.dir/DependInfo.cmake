fce1de5ef602eb9367792520b801939aa88a9da5
huagailuowen <2146964802@qq.com>
2024-07-26 12:02:14 +0800

    fick cmakelist

---
+++ src/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/周宸源/大学/学习/程序/RISC-V-Simulator-2024/src/simulator.cpp" "src/CMakeFiles/src.dir/simulator.cpp.o" "gcc" "src/CMakeFiles/src.dir/simulator.cpp.o.d"
+  "/mnt/d/周宸源/大学/学习/程序/RISC-V-Simulator-2024/src/units/Instruction.cpp" "src/CMakeFiles/src.dir/units/Instruction.cpp.o" "gcc" "src/CMakeFiles/src.dir/units/Instruction.cpp.o.d"
+  "/mnt/d/周宸源/大学/学习/程序/RISC-V-Simulator-2024/src/units/Memory.cpp" "src/CMakeFiles/src.dir/units/Memory.cpp.o" "gcc" "src/CMakeFiles/src.dir/units/Memory.cpp.o.d"
+  "/mnt/d/周宸源/大学/学习/程序/RISC-V-Simulator-2024/src/units/Register_file.cpp" "src/CMakeFiles/src.dir/units/Register_file.cpp.o" "gcc" "src/CMakeFiles/src.dir/units/Register_file.cpp.o.d"
+  "/mnt/d/周宸源/大学/学习/程序/RISC-V-Simulator-2024/src/utility/util.cpp" "src/CMakeFiles/src.dir/utility/util.cpp.o" "gcc" "src/CMakeFiles/src.dir/utility/util.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
